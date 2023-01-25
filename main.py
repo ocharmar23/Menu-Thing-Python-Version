dca62c8af84c434e46338a090eb0167a75db16f4
JoeTheDude <95007295+ocharmar23@users.noreply.github.com>
2023-01-25 00:42:22 +0000

    -Se hizo un bucle while que limpia la pantalla constantemente para mostrar algunos valores de prueba(definidos al inicio del programa). Estas variables son solo para pruebas y no figuraran demasiado en el programa.

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 from startthing import *
 import time
+unoa = 42
+dosa = 43
+tresa = 44
+cuatroa = 45
+localvalor = 0
+start()
+def someLineLol():
+  print("-------------------------")
+def progexec():
+  someLineLol()
+  localvalor = 0
+  uno = unoa
+  dos = dosa
+  tres = tresa
+  cuatro = cuatroa
+  print(f'#########  {unoa}')
+  print(f'##     ## {dosa}')
+  print(f'##     ## {dosa}')
+  print(f'#########  {unoa}')
+  print(localvalor)
+  time.sleep(1)
+  os.system('clear')
 
-def callprog():
-  start()
-
-
-callprog()
+foreverthing = True
+while foreverthing == True:
+  progexec()
+  localvalor += 1
+  unoa += 1
--- startthing.py
+++ startthing.py
@@ -1,8 +1,10 @@
-
+import os
+import time
 def start(): 
   print("##########")
   print("##      ##")
   print("##      ##")
   print("##########")
+  time.sleep(3)
   print("-------------------------------")
-  
+  os.system('clear')
